7b8b998e4a0c1eaf42c1e9b449764b431a76d420
starlitskies <majikx.prod0@gmail.com>
2023-10-29 17:49:46 +0500

    feat: transfer html overlay to js

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -25,6 +25,29 @@ const wlStatCheck = urlParams.get("wlstat") === "yes" ? "yes" : "no";
 const progressRankCheck = urlParams.get("progressrank") === "yes" ? "yes" : "no";
 const lastMatchPtsCheck = urlParams.get("lastMatchPts") === "yes" ? "yes" : "no";
 
+document.querySelector('#overlay').innerHTML = `
+  <div id="elements">
+    <div id="rankBlock">
+      <img alt="rank" src="#" id="imgRank" height="80" width="80" />
+    </div>
+    <div id="playerInfo">
+      <h2 style="text-transform: uppercase" id="mainText">
+        Rating
+      </h2>
+      <h1 style="text-transform: uppercase" id="playerRank"></h1>
+      <div id="wlstat">
+        <h2 id="WLvalue">Win: <span id="winValue"></span> Lose: <span id="loseValue"></span> <span id="wlProccent"></span></h2>
+      </div>
+      <div id="progressrank"></div>
+      <div class="lastmatchpts">
+        <span id="lastmatchpts">Last Match:</span>
+        <span id="lastmatchptsvalue"></span>
+        <img src="#" id="imgPTS" height="16" width="16"/>
+      </div>
+    </div>
+  </div>
+`
+
 // Elements
 const imgRank = document.getElementById("imgRank");
 const imgPTS = document.getElementById("imgPTS");
