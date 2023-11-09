915bce0d0a7552181e4ca37f01e223107be01274
starlitskies <majikx.prod0@gmail.com>
2023-11-09 05:49:27 +0500

    chore(vite): add __dirname

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { resolve } from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
   plugins: [],
