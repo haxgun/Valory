b53d3e44e2b63a3b8d9ab5993a80e49b900548d1
MAGICX <majikx.prod0@gmail.com>
2024-12-03 05:36:45 +0500

    fix: add uvicorn in import and rm slash in include_router

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
@@ -30,7 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_router, prefix="/api/")
+app.include_router(api_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
