9feab1ce1a5bd2ab1a1c4bd4206739486163bffb
MAGICX <majikx.prod0@gmail.com>
2024-12-06 00:44:01 +0500

    chore(backend/api): rename overlay to overlays

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import overlay
 
 api_router = APIRouter()
-api_router.include_router(overlay.router, prefix="/overlay", tags=["overlay"])
+api_router.include_router(overlay.router, prefix="/overlays", tags=["overlays"])
