2783f477d6e5fcf9ea8a09752e41e24d2824e711
Misha Gusev <majikx.prod0@gmail.com>
2024-12-29 07:53:02 +0500

    chore: add color prop for valory icon

--- frontend/src/components/icons/IconValory.vue
+++ frontend/src/components/icons/IconValory.vue
@@ -4,6 +4,10 @@ defineProps({
     type: Number,
     default: 24,
   },
+  color: {
+    type: String,
+    default: '#FA4454',
+  },
 })
 </script>
 
@@ -20,11 +24,11 @@ defineProps({
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M339.142 6L51 162.9V326.506L339.142 479.383L627.285 326.506V162.9L339.142 6ZM339.431 136.682L205.247 209.576V285.585L339.431 356.61L473.615 285.585V209.576L339.431 136.682Z"
-        fill="#FA4454"
+        :fill="color"
       />
       <path
         d="M339.5 548.547L628 395.906V520.697L339.5 673.337L51 520.697V395.906L339.5 548.547Z"
-        fill="#FA4454"
+        :fill="color"
       />
     </svg>
   </span>
