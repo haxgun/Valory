d052df5f49517f8a844f73d26b373a47aa9d863c
Misha Gusev <majikx.prod0@gmail.com>
2024-12-29 07:30:29 +0500

    feat: little changes for unsupported page

--- frontend/src/views/UnsupportedView.vue
+++ frontend/src/views/UnsupportedView.vue
@@ -1,15 +1,27 @@
-<script setup lang="ts"></script>
 <template>
-  <div class="unsupported">
-    <h1>Unsupported Device</h1>
-    <p>This website is not available on mobile devices. Please use a desktop browser.</p>
-  </div>
+  <main>
+    <div class="main__body">
+      <div class="main__container">
+        <h1>Unsupported Device</h1>
+        <p>This website is not available on mobile devices. Please use a desktop browser.</p>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-.unsupported {
-  text-align: center;
-  margin-top: 50px;
-  color: #333;
+<style lang="scss" scoped>
+.title {
+  margin: 0;
+  font-weight: bold;
+  font-size: 34px;
+}
+
+.subtitle {
+  margin: 0;
+  font-weight: 300;
+  font-size: 15px;
+  color: #9f9f9f;
+  position: relative;
+  top: -20px;
 }
 </style>
