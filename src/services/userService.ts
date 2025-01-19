cc66c98dd3cc91e0536063ec01e653c310150d84
Misha Gusev <majikx.prod0@gmail.com>
2025-01-19 14:08:56 +0500

    fix: setup puuid in get puuid function api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -23,11 +23,18 @@ export const getPuuid = async (): Promise<boolean> => {
     settingsStore.puuid = null;
     return false;
   }
+
   const { name, tag } = extractNameAndTag(riotId) || { name: '', tag: '' };
+
+  if (!name || !tag) {
+    console.error('Invalid Riot ID:', riotId);
+    settingsStore.puuid = null;
+    return false;
+  }
 
   try {
     const response = await apiClient.get(`/v2/account/${name}/${tag}`);
-    settingsStore.puuid = response.data.puuid;
+    settingsStore.puuid = response.data.data.puuid;
     return true;
   } catch (error) {
     console.error('Error getting the PUUID:', error);
