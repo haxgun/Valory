0a2421dbff88351b613bde484456fbfbd69e19d9
Misha Gusev <majikx.prod0@gmail.com>
2025-02-03 03:14:17 +0500

    fix(backend): double routing for auth

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.routers import overlays, auth
+from app.routers import overlays
+from app.routers.auth import twitch
 
 api_router = APIRouter()
 api_router.include_router(overlays.router, prefix="/overlay", tags=["overlay"])
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(twitch.router, prefix="/auth/twitch", tags=["auth"])
--- backend/app/routers/auth/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.routers.auth import twitch
-from fastapi import APIRouter
-
-router = APIRouter()
-router.include_router(twitch.router, prefix="/twitch", tags=["twitch"])
